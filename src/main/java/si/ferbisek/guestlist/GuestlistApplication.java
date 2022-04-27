44dc6596c82460eb82ebb69071222619c5aa0437
Goran Ferbišek <goran.ferbisek@gmail.com>
2022-04-27 15:53:49 +0200

    New Spring Boot application using Spring Initializr

---
+++ src/main/java/si/ferbisek/guestlist/GuestlistApplication.java
@@ -0,0 +1,13 @@
+package si.ferbisek.guestlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/ferbisek/guestlist/GuestlistApplicationTests.java
@@ -0,0 +1,13 @@
+package si.ferbisek.guestlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
